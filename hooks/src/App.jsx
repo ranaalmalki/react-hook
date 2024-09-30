901c63ca3cb09dfcee666bac31baf3d552a20ec4
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-30 12:40:49 +0300

    add the netlfy and add show hide img

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -14,6 +14,8 @@ function App() {
 
   const [color , setColor] = useState("")
   const [colorbg , setColorbg] = useState("")
+
+  const [imgchange , setImgchange] =useState(false)
   // setColorbg("black")
   return (
     <div style={{backgroundColor:colorbg,color:color}}>
@@ -40,13 +42,16 @@ function App() {
 <button onClick={()=>setSub(num1 - num2)}>Sub</button>
 <button onClick={()=>setMul(num1 * num2)}>Multipale</button>
 <button onClick={()=>setDiv(num1 % num2)}>divied</button>
-<div>
-<h1>Sum: {add}</h1>
-<h1>Sub:{sub}</h1>
-<h1>Multipale:{mul}</h1>
-<h1>Divied:{div}</h1>
+<div className='flex gap-4 p-10'>
+<h3 className='card shadow-md shadow-emerald-300'>Sum: {add}</h3>
+<h3 className='card shadow-md shadow-red-200'>Sub:{sub}</h3>
+<h3 className='card shadow-md shadow-yellow-200'>Multipale:{mul}</h3>
+<h3 className='card shadow-md shadow-lime-200'>Divied:<span>{div}</span></h3>
 </div>
-    
+    <button onClick={()=>setImgchange(!imgchange)}>{imgchange ?'Hide content':'show content'}</button>
+    {imgchange &&(
+    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8ef9b039-d5e7-44d1-a702-75c18817ce3c/d6cgngj-7596d761-bf5d-4b50-853e-546869ad7dc4.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhlZjliMDM5LWQ1ZTctNDRkMS1hNzAyLTc1YzE4ODE3Y2UzY1wvZDZjZ25nai03NTk2ZDc2MS1iZjVkLTRiNTAtODUzZS01NDY4NjlhZDdkYzQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.fSKdKXtwMAcAQLl9p6DgqcFw0ZMCgdG4HHdYTIXrTaY" alt=""/>
+  )}
     </div>
   )
 }
